9585749908cc53081327d30bd08142a705941417
Peter Jin <peterhaijin@gmail.com>
2018-12-24 20:11:44 -0800

    Working on a minimal example.

--- build.rs
+++ build.rs
@@ -1,4 +1,19 @@
 extern crate nvptxglue;
 
+use nvptxglue::prelude::*;
+
 fn main() {
+  nvptxglue::Builder::default()
+    .crate_dir("kernels")
+    .gencode(Gencode::Ptx(Cc_3_5))
+    //.gencode(Gencode::Sass(Cc_3_5))
+    //.gencode(Gencode::Sass(Cc_5_2))
+    //.gencode(Gencode::Sass(Cc_6_0))
+    //.gencode(Gencode::Sass(Cc_6_1))
+    .whitelist_kernel("hello_kernel")
+    .generate()
+    .expect("failed to generate glue")
+    .write_to_file("kernels_glue.rs")
+    .expect("failed to write glue to file");
+  //panic!();
 }
---
+++ kernels/lib.rs
@@ -0,0 +1,28 @@
+#![feature(abi_ptx)]
+#![feature(core_intrinsics)]
+#![feature(stdsimd)]
+#![no_std]
+
+use core::arch::nvptx::*;
+
+#[no_mangle]
+pub unsafe extern "ptx-kernel" fn hello_kernel(x: *mut u32) {
+  let idx = _thread_idx_x() as isize;
+  match idx {
+    0 => {
+      *x.offset(idx) = 0x_6c_6c_65_48;
+    }
+    1 => {
+      *x.offset(idx) = 0x_77_20_2c_6f;
+    }
+    2 => {
+      *x.offset(idx) = 0x_64_6c_72_6f;
+    }
+    3 => {
+      *x.offset(idx) = 0x_00_00_0a_21;
+    }
+    _ => {
+      *x.offset(idx) = 0;
+    }
+  }
+}
