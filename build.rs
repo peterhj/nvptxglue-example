4b798d1f8c51d826854c4779fe2a83e5c86e79c4
Peter Jin <peterhaijin@gmail.com>
2018-12-28 12:21:03 -0800

    Use the new `Builder` API.

--- build.rs
+++ build.rs
@@ -2,10 +2,7 @@ extern crate nvptxglue;
 
 use nvptxglue::prelude::*;
 
-//use std::env;
-
 fn main() {
-  //let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
   nvptxglue::Builder::default()
     .crate_dir("kernels")
     //.gencode(Gencode::VGpu(Cc_3_5))
@@ -14,9 +11,8 @@ fn main() {
     //.gencode(Gencode::Gpu(Cc_6_0))
     //.gencode(Gencode::Gpu(Cc_6_1))
     .whitelist_kernel("hello_kernel")
-    .generate()
-    .expect("failed to generate glue")
-    //.write_to_file(out_dir.join("kernels_glue.rs"))
-    .write_to_file(CudaFfiGlue::default(), "src/kernels_glue.rs")
-    .expect("failed to write glue to file");
+    .compile_fatbin()
+    .expect("failed to compile kernels")
+    .write_bindings_to_file(CudaFfiGlue::default(), "src/kernels_glue.rs")
+    .expect("failed to generate glue bindings");
 }
