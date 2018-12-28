d485de479a033f77d03fc9a73549baca1bfa5648
Peter Jin <peterhaijin@gmail.com>
2018-12-28 15:44:01 -0800

    Use latest builder API. Remove unused deps.

--- build.rs
+++ build.rs
@@ -5,13 +5,13 @@ use nvptxglue::prelude::*;
 fn main() {
   nvptxglue::Builder::default()
     .crate_dir("kernels")
-    //.gencode(Gencode::VGpu(Cc_3_5))
-    .gencode(Gencode::Gpu(Cc_3_5))
+    .gencode(Gencode::Ptx(Cc_3_5))
+    //.gencode(Gencode::Gpu(Cc_3_5))
     //.gencode(Gencode::Gpu(Cc_5_2))
     //.gencode(Gencode::Gpu(Cc_6_0))
     //.gencode(Gencode::Gpu(Cc_6_1))
     .whitelist_kernel("hello_kernel")
-    .compile_fatbin()
+    .compile(Phase::Fatbin)
     .expect("failed to compile kernels")
     .write_bindings_to_file(CudaFfiGlue::default(), "src/kernels_glue.rs")
     .expect("failed to generate glue bindings");
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 extern crate cuda_ffi_types;
 extern crate cuda;
-#[macro_use] extern crate lazy_static;
 
 pub mod kernels_glue;
